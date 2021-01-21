ef3cdd726294c950d3c25be42a231259c6121770
didisouzacosta <adrianosouzacostaios@gmail.com>
2021-01-21 07:04:54 -0300

    Setup inicial do projeto

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '11.4'
+
+target 'Pokédex MVP' do
+  use_frameworks!
+
+  # Pods for Pokédex MVP
+
+end
---
+++ Pokédex MVP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokédex MVP
+//
+//  Created by Adriano Souza Costa on 21/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
