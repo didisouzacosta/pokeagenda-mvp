f4070f1559ec9af1f630428c2955b9b19209a318
didisouzacosta <adrianosouzacostaios@gmail.com>
2021-01-22 08:00:51 -0300

    Criado arquivo com os padrões tipográficos

--- Pokedex-MVP/Main/Utils/Typography.swift
+++ Pokedex-MVP/Main/Utils/Typography.swift
@@ -5,6 +5,16 @@
 //  Created by Adriano Souza Costa on 21/01/21.
 //
 
-import Foundation
+import UIKit
 
-struct Typography {}
+struct Typography {
+    
+    static let title = UIFont.boldSystemFont(ofSize: 100)
+    static let applicationTitle = UIFont.boldSystemFont(ofSize: 32)
+    static let pokemonName = UIFont.boldSystemFont(ofSize: 26)
+    static let filterName = UIFont.boldSystemFont(ofSize: 16)
+    static let description = UIFont.systemFont(ofSize: 16)
+    static let pokemonNumber = UIFont.boldSystemFont(ofSize: 12)
+    static let pokemonType = UIFont.systemFont(ofSize: 12, weight: .medium)
+    
+}
