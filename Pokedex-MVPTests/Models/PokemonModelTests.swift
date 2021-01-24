b333bd44f6429b15ed19f178aae384026d170ce6
didisouzacosta <adrianosouzacostaios@gmail.com>
2021-01-24 08:25:56 -0300

    Cria teste para o decode do model de pokemon

---
+++ Pokedex-MVPTests/Models/PokemonModelTests.swift
@@ -0,0 +1,57 @@
+//
+//  PokemonModelTests.swift
+//  Pokedex-MVPTests
+//
+//  Created by Adriano Souza Costa on 24/01/21.
+//
+
+import XCTest
+import Nimble
+
+@testable import Pokedex_MVP
+
+class PokemonModelTests: XCTestCase {
+    
+    func testEnsureDecodePokemonModel() throws {
+        let json = """
+        {
+            "sprites": {
+                "other": {
+                    "official-artwork": {
+                        "front_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+                    }
+                }
+            },
+            "types": [
+                {
+                  "slot": 1,
+                  "type": {
+                    "name": "grass",
+                    "url": "https://pokeapi.co/api/v2/type/12/"
+                  }
+                },
+                {
+                  "slot": 2,
+                  "type": {
+                    "name": "poison",
+                    "url": "https://pokeapi.co/api/v2/type/4/"
+                  }
+                }
+              ],
+              "weight": 69
+        }
+        """.data(using: .utf8)!
+        
+        let decoder = JSONDecoder()
+        let pokemon = try decoder.decode(Pokemon.self, from: json)
+        
+        expect(pokemon.sprites.other.officialArtwork.frontDefault.absoluteString) == "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+        expect(pokemon.types.count) == 2
+        expect(pokemon.types[0].slot) == 1
+        expect(pokemon.types[0].type.name) == "grass"
+        expect(pokemon.types[1].slot) == 2
+        expect(pokemon.types[1].type.name) == "poison"
+        expect(pokemon.weight) == 69
+    }
+    
+}
