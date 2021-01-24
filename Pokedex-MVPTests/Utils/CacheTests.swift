f0816b56b355385cb0091280a479ba8beba88d59
didisouzacosta <adrianosouzacostaios@gmail.com>
2021-01-24 13:26:32 -0300

    Cria testes para o utils de cache

--- Pokedex-MVPTests/Utils/CacheTests.swift
+++ Pokedex-MVPTests/Utils/CacheTests.swift
@@ -23,4 +23,39 @@ class CacheTests: XCTestCase {
         expect(cachedData) == "Zelda >> Pokemon"
     }
     
+    func testRemoveCachedObject() throws {
+        let cache = Cache()
+        let data = ["iOS", "Android"]
+        
+        try cache.set(data, key: "platforms")
+        
+        var cachedData = try cache.get(type: [String].self, key: "platforms")
+        
+        expect(cachedData) == ["iOS", "Android"]
+        
+        cache.remove(key: "platforms")
+        
+        cachedData = try cache.get(type: [String].self, key: "platforms")
+        
+        expect(cachedData).to(beNil())
+    }
+    
+    func testClearCacheData() throws {
+        let cache = Cache()
+        
+        try cache.set(["iOS", "Android"], key: "platforms")
+        try cache.set(["React Native", "Flutter"], key: "hibrid")
+        try cache.set(["Fullmetal Alchimist", "Dead Note"], key: "best-animes")
+        
+        cache.clear()
+        
+        let bestAnimes = try cache.get(type: [String].self, key: "best-animes")
+        let hibrid = try cache.get(type: [String].self, key: "hibrid")
+        let platforms = try cache.get(type: [String].self, key: "platforms")
+        
+        expect(bestAnimes).to(beNil())
+        expect(hibrid).to(beNil())
+        expect(platforms).to(beNil())
+    }
+    
 }
